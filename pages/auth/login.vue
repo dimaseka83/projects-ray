a26980e96ba15ea2231179ca4912b902f6b9b86d
dhimaseka <dimaseka83@gmail.com>
2022-05-23 09:35:36 +0700

    ++

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,131 +1,89 @@
 <template>
-  <v-container class="fill-height" fluid>
+  <v-container class="fill-height full-background" fluid>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="8" md="8">
-        <v-card class="elevation-12">
+        <v-card class="elevation-12 rounded-xl">
           <v-window v-model="step">
             <v-window-item :value="1">
               <v-row>
-                <v-col cols="12" md="8">
+                <v-col cols="12" md="6" class="background">
+                </v-col>
+                  <v-col cols="12" md="6">
                   <v-card-text class="mt-12">
-                    <h1 class="text-center display-2 teal--text text--accent-3">Sign in to Account</h1>
-                    <div class="text-center mt-4">
-                      <v-btn class="mx-2" fab color="primary">
-                        <v-icon dark>
-                          mdi-facebook
-                        </v-icon>
-                      </v-btn>
-                      <v-btn class="mx-2" fab color="red">
-                        <v-icon dark>
-                          mdi-google-plus
-                        </v-icon>
-                      </v-btn>
-                      <v-btn class="mx-2" fab color="primary">
-                        <v-icon dark>
-                          mdi-linkedin
-                        </v-icon>
-                      </v-btn>
+                    <div class="text-center">
+                      <img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" height="100">
                     </div>
-                    <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                    <h1 class="text-center display-1 primary--text text--accent-3">Selamat Datang di NuxtJS</h1>
+                    <h4 class="text-center mt-4 ">Mohon isi kolom Email dan Password untuk masuk </h4>
                     <v-form>
                       <v-text-field label="Email" name="Email" prepend-icon="mdi-email" type="text"
-                        color="teal accent-3" />
+                        color="primary accent-3" />
 
                       <v-text-field id="password" label="Password" name="password" prepend-icon="mdi-lock"
-                        type="password" color="teal accent-3" />
+                        type="password" color="primary accent-3" />
                     </v-form>
-                    <h5 class="text-center mt-4">Forgot your password ? <v-btn text color="primary" @click="step += 2">
-                        Click Here</v-btn>
+                    <h5 class="text-center mt-4">Belum punya akun? <v-btn text color="primary" @click="step++">
+                        Daftar</v-btn>
                     </h5>
                   </v-card-text>
-                  <div class="text-center m-3">
-                    <v-btn rounded color="teal accent-3" dark @click="login">SIGN IN</v-btn>
-                  </div>
-                </v-col>
-                <v-col cols="12" md="4" class="teal accent-3">
-                  <v-card-text class="white--text mt-12">
-                    <h1 class="text-center display-1">Hello, Friend!</h1>
-                    <h5 class="text-center">Enter your personal details and start journay with us</h5>
-                  </v-card-text>
-                  <div class="text-center">
-                    <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                  <div class="d-flex m-3">
+                    <v-btn text @click="step += 2" color="blue">Forgot Password?</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary accent-3" dark @click="login">Login</v-btn>
                   </div>
                 </v-col>
               </v-row>
             </v-window-item>
             <v-window-item :value="2">
-              <v-row class="fill-height">
-                <v-col cols="12" md="4" class="teal accent-3">
-                  <v-card-text class="white--text mt-12">
-                    <h1 class="text-center display-1">Welcome Back!</h1>
-                    <h5 class="text-center">To Keep connected with us please login with your personnel info</h5>
-                  </v-card-text>
-                  <div class="text-center">
-                    <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
-                  </div>
+                           <v-row>
+                <v-col cols="12" md="6" class="background">
                 </v-col>
-
-                <v-col cols="12" md="8">
+                  <v-col cols="12" md="6">
                   <v-card-text class="mt-12">
-                    <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
-                    <div class="text-center mt-4">
-                      <v-btn class="mx-2" fab color="primary">
-                        <v-icon dark>
-                          mdi-facebook
-                        </v-icon>
-                      </v-btn>
-                      <v-btn class="mx-2" fab color="red">
-                        <v-icon dark>
-                          mdi-google-plus
-                        </v-icon>
-                      </v-btn>
-                      <v-btn class="mx-2" fab color="primary">
-                        <v-icon dark>
-                          mdi-linkedin
-                        </v-icon>
-                      </v-btn>
+                    <div class="text-center">
+                      <img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" height="100">
                     </div>
-                    <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                    <h1 class="text-center display-1 primary--text text--accent-3">Selamat Datang di NuxtJS</h1>
+                    <h4 class="text-center mt-4 ">Mohon isi kolom Email dan Password untuk masuk </h4>
                     <v-form>
-                      <v-text-field label="Name" name="Name" prepend-icon="mdi-account" type="text"
-                        color="teal accent-3" />
                       <v-text-field label="Email" name="Email" prepend-icon="mdi-email" type="text"
-                        color="teal accent-3" />
+                        color="primary accent-3" />
 
-                      <v-text-field id="password" label="Password" name="" prepend-icon="mdi-lock" type="password"
-                        color="teal accent-3" />
+                      <v-text-field id="password" label="Password" name="password" prepend-icon="mdi-lock"
+                        type="password" color="primary accent-3" />
                     </v-form>
+                    <h5 class="text-center mt-4">Sudah Punya Akun? <v-btn text color="primary" @click="step--">
+                        Login</v-btn>
+                    </h5>
                   </v-card-text>
-                  <div class="text-center m-5">
-                    <v-btn rounded color="teal accent-3" dark @click="signup">SIGN UP</v-btn>
+                  <div class="text-center m-3">
+                    <v-btn block color="primary accent-3" dark @click="login">Daftar</v-btn>
                   </div>
                 </v-col>
               </v-row>
             </v-window-item>
             <v-window-item :value="3">
-              <v-row class="fill-height">
-                <v-col cols="12" md="4" class="teal accent-3">
-                  <v-card-text class="white--text mt-12">
-                    <h1 class="text-center display-1">Welcome Back!</h1>
-                    <h5 class="text-center">To Keep connected with us please login with your personnel info</h5>
-                  </v-card-text>
-                  <div class="text-center">
-                    <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
-                  </div>
+                          <v-row>
+                <v-col cols="12" md="6" class="background">
                 </v-col>
-
-                <v-col cols="12" md="8">
+                  <v-col cols="12" md="6">
                   <v-card-text class="mt-12">
-                    <h5 class="text-center display-1 teal--text text--accent-3">Did you forgot your password</h5>
-                    <p class="text-center mt-4">Enter your email address you're using for your account bellow and we
-                      will send you a password reset link</p>
+                    <div class="text-center">
+                      <img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" height="100">
+                    </div>
+                    <h1 class="text-center display-1 primary--text text--accent-3">Lupa Password</h1>
+                    <h4 class="body-2 text-center mt-4 ">Enter your email address you're using for your account bellow and we
+                      will send you a password reset link</h4>
                     <v-form>
                       <v-text-field label="Email" name="Email" prepend-icon="mdi-email" type="text"
-                        color="teal accent-3" />
+                        color="primary accent-3" />
                     </v-form>
+                    <h5 class="text-center mt-4">Belum punya akun? <v-btn text color="primary" @click="step++">
+                        Daftar</v-btn>
+                    </h5>
                   </v-card-text>
-                  <div class="text-center m-5">
-                    <v-btn rounded color="teal accent-3" dark @click="forgotpassword">Register Reset Link</v-btn>
+                  <div class="text-center m-3">
+                    <v-btn block color="primary accent-3" dark @click="forgotpassword">Reset Password</v-btn>
                   </div>
                 </v-col>
               </v-row>
@@ -184,3 +142,16 @@
   };
 
 </script>
+<style>
+.full-background {
+  background-image: url('~/assets/images/background.svg');
+  height: 100%;
+   background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.background{
+  background-image: url('~/assets/images/background.svg');
+
+}
+</style>
